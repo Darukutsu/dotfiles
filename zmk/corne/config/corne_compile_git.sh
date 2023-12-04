073275ccf8f9923fc0bb187b5ba28b8433077b6c
Darukutsu <darupeter@pm.me>
2023-12-04 09:52:03 +0100

    add corne

---
+++ zmk/corne/config/corne_compile_git.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# I run this script from "$HOME"/.config/zmk/zmk/app
+# build/left build/right should be renamed to abs path
+# to be able to run from anywhere...i guess
+
+source "$HOME"/.config/zmk/zmk/python-dep/bin/activate
+west build -b nice_nano_v2 -d build/left -- -DSHIELD="corne_left" -DZMK_CONFIG="$HOME/.config/zmk/corne/config"
+west build -b nice_nano_v2 -d build/right -- -DSHIELD="corne_right" -DZMK_CONFIG="$HOME/.config/zmk/corne/config"
+
+cp -f "$HOME"/.config/zmk/zmk/app/build/left/zephyr/zmk.uf2 zmk_left.uf2
+cp -f "$HOME"/.config/zmk/zmk/app/build/right/zephyr/zmk.uf2 zmk_right.uf2
