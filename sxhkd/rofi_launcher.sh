b9a1a291f689ab48c6eaf51b0c9dde38efd45e69
Darukutsu <darupeter@pm.me>
2024-04-13 13:10:52 +0200

    add and fix some nnn plugins, fix sxhkd keybind modes

--- sxhkd/rofi_launcher.sh
+++ sxhkd/rofi_launcher.sh
@@ -23,6 +23,7 @@ if [ "$1" = "combi" ];then
   elif [ "$1" = "powermenu" ];then
   rofi-applet-powermenu "$theme_applet"
   elif [ "$1" = "sxhkd_help" ];then
+  # TODO: parse multiple files
   awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc |
   column -t -s $'\t' |
   rofi  -dmenu -i -markup-rows -no-show-icons -width 1000 -lines 15 -yoffset 40 -normal-window
