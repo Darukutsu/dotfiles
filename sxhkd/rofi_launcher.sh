e4d592be963706ca9546312fd4ffb35834b218af
Darukutsu <darupeter@pm.me>
2024-06-26 15:52:04 +0200

    add X/Wayland clipboard

--- sxhkd/rofi_launcher.sh
+++ sxhkd/rofi_launcher.sh
@@ -4,29 +4,29 @@
 theme_applet="$XDG_CONFIG_HOME/rofi/applet.rasi"
 
 ## Run
-if [ "$1" = "combi" ];then
+if [ "$1" = "combi" ]; then
   #rofi -modi "combi,emoji,calc" -show "combi"  -emoji-file "$path" -normal-window
   rofi -modi "combi,calc" -show "combi" -combi-modes run -normal-window
-  elif [ "$1" = "window" ];then
-  rofi -show window  -normal-window
-  elif [ "$1" = "hidden-window" ];then
+elif [ "$1" = "window" ]; then
+  rofi -show window -normal-window
+elif [ "$1" = "hidden-window" ]; then
   rofi_hidden_window
-  elif [ "$1" = "clipboard" ];then
-  rofi-gpaste
-  elif [ "$1" = "layout" ];then
+elif [ "$1" = "clipboard" ]; then
+  rofi-clipboard
+elif [ "$1" = "layout" ]; then
   rofi-bsp-layout $2
-  elif [ "$1" = "screenshot" ];then
+elif [ "$1" = "screenshot" ]; then
   rofi-applet-screenshot "$theme_applet"
-  elif [ "$1" = "bitwarden" ];then
+elif [ "$1" = "bitwarden" ]; then
   #bwmenu
   rofi-rbw
-  elif [ "$1" = "powermenu" ];then
+elif [ "$1" = "powermenu" ]; then
   rofi-applet-powermenu "$theme_applet"
-  elif [ "$1" = "sxhkd_help" ];then
+elif [ "$1" = "sxhkd_help" ]; then
   # TODO: parse multiple files
   awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc |
-  column -t -s $'\t' |
-  rofi  -dmenu -i -markup-rows -no-show-icons -width 1000 -lines 15 -yoffset 40 -normal-window
+    column -t -s $'\t' |
+    rofi -dmenu -i -markup-rows -no-show-icons -width 1000 -lines 15 -yoffset 40 -normal-window
 else
   echo "unknown option"
 fi
