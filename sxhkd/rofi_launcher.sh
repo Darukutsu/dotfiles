cbec3ebb3712fc0c520e699c0a4ca4b1e6c9b524
Darukutsu <darupeter@pm.me>
2025-02-15 11:29:15 +0100

    code cleanup

--- sxhkd/rofi_launcher.sh
+++ sxhkd/rofi_launcher.sh
@@ -6,9 +6,9 @@ theme_applet="$XDG_CONFIG_HOME/rofi/applet.rasi"
 ## Run
 if [ "$1" = "combi" ]; then
   #rofi -modi "combi,emoji,calc" -show "combi"  -emoji-file "$path" -normal-window
-  rofi -modi "combi,calc" -show "combi" -combi-modes run,drun -normal-window
+  rofi -modi "combi,calc" -show "combi" -combi-modes run,drun #-normal-window
 elif [ "$1" = "window" ]; then
-  rofi -show window -normal-window
+  rofi -show window #-normal-window
 elif [ "$1" = "hidden-window" ]; then
   rofi_hidden_window
 elif [ "$1" = "clipboard" ]; then
@@ -26,7 +26,7 @@ elif [ "$1" = "sxhkd_help" ]; then
   # TODO: parse multiple files
   awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc |
     column -t -s $'\t' |
-    rofi -dmenu -i -markup-rows -no-show-icons -width 1000 -lines 15 -yoffset 40 -normal-window
+    rofi -dmenu -i -markup-rows -no-show-icons -width 1000 -lines 15 -yoffset 40 #-normal-window
 else
   echo "unknown option"
 fi
